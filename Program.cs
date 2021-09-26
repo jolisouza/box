7cc833df05fc5bcb2934c8613e1b771872e5bffc
joliathan souza <coelhodesouzajoliathan1@gmail.com>
2021-09-26 16:57:07 -0300

    resollução do exercício

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.clear();
+        }
+            Console.WriteLine();
+            Console.WriteLine("+-----------------+");
+            Console.WriteLine("| joliathan souza |");
+            Console.WriteLine("+-----------------+");
+            Console.WriteLine();
+        }
+    }
+}
+
